55659d903a1504764414a28107325f435deb8b5f
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-03-31 21:03:47 -0400

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,156 @@
+/****************************************************************************
+ * * Created by: Heejo Suh
+ * Created on: Mar 2018
+ * Created for: learning
+ * 
+ * This class is a 'blueprint' for a stack
+ * 
+ * 
+ ****************************************************************************/
+
+
+public class Triangle{
+	//class for triangle
+	
+	//Private Fields
+	
+	private static double _sideA;
+	private static double _sideB;
+	private static double _sideC;
+	
+	private double _semiperimeter;
+	private double _perimeter;
+	
+	private double _area;
+	private String _typeName;
+	
+
+	
+	//CONSTRUCTOR ----------------------------------------------------------------------------------
+	public Triangle(double a, double b, double c){
+		//Pass parameters
+		
+		_sideA = a;
+		_sideB = b;
+		_sideC = c;
+		
+		_semiperimeter = 0.5 * (this.getPerimeter());
+		
+	} 
+
+	
+	
+	
+	//TRIANGLE VALID------------------------------------------------------------------------------------------
+	private double round(double inputDouble){
+		//returns rounded number
+		return Double.valueOf(String.format("%.2f", inputDouble));
+	}	
+	
+
+	//PERIMETER--------------------------------------------------------------------------------------------------
+	private double getPerimeter(){
+		//calculates and returns perimeter
+		_perimeter = _sideA + _sideB +_sideC;
+		return _perimeter;
+	}
+	
+	
+	
+	
+	//TRIANGLE VALID------------------------------------------------------------------------------------------
+	protected boolean getTriangleValidity(){
+		//check if triangle is valid or not
+		
+		boolean triangleValidity;
+		
+		if (_sideA + _sideB > _sideC && _sideA + _sideC > _sideB && _sideB + _sideC > _sideA){
+			//sum of two sides are greater than the other
+			triangleValidity = true;
+			
+		} else {
+			triangleValidity = false;
+		}
+
+		return triangleValidity;
+	}
+
+	
+	
+
+	//AREA OF TRIANGLE-----------------------------------------------------------------------------------------
+	public double GetArea() { 
+		//calculates and returns the area
+		
+		//area = ( s(s-a)(s-b)(s-c) ) ^ 0.5
+		_area = Math.sqrt((_semiperimeter*(_semiperimeter - _sideA)*(_semiperimeter - _sideB)*(_semiperimeter - _sideC)));
+		//return Math.round(_area);
+		return round(_area);
+	}		
+
+	//TRIANGLE TYPE-----------------------------------------------------------------------------------------------
+	public String GetName() { 
+		//calculates and returns the triangle type
+		
+		//Math.pow(side,2)
+		if ((_sideA*_sideA)==(_sideB*_sideB)+(_sideC*_sideC) || (_sideB*_sideB)==(_sideA*_sideA)+(_sideC*_sideC) || (_sideC*_sideC)==(_sideA*_sideA)+(_sideB*_sideB)){
+		//if (Math.pow(_sideA,2)==Math.pow(_sideB,2)+Math.pow(_sideC,2) || Math.pow(_sideB,2)==Math.pow(_sideA,2)+Math.pow(_sideC,2) || Math.pow(_sideC,2)==Math.pow(_sideA,2)+Math.pow(_sideB,2)){
+			//right angle
+			_typeName = "Right-angle";
+			
+		} else if (_sideA == _sideB && _sideA == _sideC){
+			//all sides equal
+			_typeName = "Equilateral";
+			
+		} else if (_sideA == _sideB || _sideB == _sideC || _sideA == _sideC){
+			//two sides equal
+			_typeName = "Isosceles";
+			
+		} else {
+			//else
+			_typeName = "Scalene";
+		}
+		
+		
+		return _typeName;
+	}		
+	
+	
+	//HEIGHT-----------------------------------------------------------------------------------------------------
+	public void printTriangleHeight(){
+		//calculates and prints the heights
+		//A = 0.5*bh
+		//h = 2A / b
+		double heightA = _area*2 / _sideA;
+		double heightB = _area*2 / _sideB;
+		double heightC = _area*2 / _sideC;
+		
+		//just print here-> less work
+		System.out.println("Heights: " + round(heightA) + ", " + round(heightB) + ", " + round(heightC));
+	}
+	
+	//LARGEST INSCRIBED CIRCLE---------------------------------------------------------------------------------------
+	public void printLargestInscribedCircle(){
+		//calculates and prints the radius of the largest inscribed circle
+		
+		//radius= area of triangle / semiperimeter
+		double radius = _area / _semiperimeter;
+		
+		System.out.println("The radius of the largest inscribed circle that can fit inside the triangle: " + round(radius));
+		
+	}
+	
+	//CIRCMCIRCLE----------------------------------------------------------------------------------------------------
+	public void printAreaOfTheCircumcircleOfTheTriangle(){
+		//calculates and returns the circumcircle
+		//radius = (a+b+c)/(4*Area)
+		double radiusOfCircumcircleOfTheTriangle = _sideA*_sideB*_sideC / (4 * _area);
+		double areaOfCircumcircleOfTheTriangle = 3.14 * Math.pow(radiusOfCircumcircleOfTheTriangle, 2);
+		
+		System.out.println("The area of the circumcircle of the triangle: " + round(areaOfCircumcircleOfTheTriangle));
+	}
+	//----------------------------------------------------------------------------------------------------
+	
+}//closing for class
+
+
---
+++ TriangleCalculator.java
@@ -0,0 +1,88 @@
+/****************************************************************************
+ *
+ * Created by: Heejo Suh
+ * Created on: Mar 2018
+ * Created for: learning
+ * 
+ * This program uses the stack 'Triangle',
+ * to calculate values of input triangle
+ *
+ ****************************************************************************/
+
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+
+//****************************************************************************
+public class TriangleCalculator {
+	//This class tests the stack
+	
+	//-----------------------
+	public static void main(String[] args) throws Exception {
+		//instantiate triangle given the input and calculate values
+		
+		Scanner scanner = new Scanner(System.in);
+		String input = "", choice = "";
+		
+        //Hashtable<String, Double> inputValues = new Hashtable<String, Double>();
+		//Map<String, Double> inputValues = new HashMap<String, Double>();
+		
+		//create list
+		List<Double> inputValues = new ArrayList<>();
+
+		
+		//--------------------------------------------------------------------------
+		//Get correct inputs -------------------------------------------------------
+		while (inputValues.size() < 2) {
+			//not have exited program
+			
+			//get number----------------------------------------
+			for(int count = 0 ; count<3; count++) {
+				//see if input is a number
+				System.out.println("\nInsert the length of a side: ");
+				
+				try {
+					//check if response is a number
+					input = scanner.nextLine();
+					double inputNumber = Double.valueOf(input);
+					if (inputNumber < 0 ) {
+						System.out.println("Insert a valid number!");
+					} else {
+						//insert into dictionary
+						inputValues.add(inputNumber);
+					}	
+					//need to reset to default
+					input = "";
+					
+				}catch (IllegalArgumentException x) { 
+					System.out.println("Insert a number!");
+				}
+			}
+		} 
+		
+		
+		
+		//instantiate Triangle
+		Triangle customTriangle = new Triangle(inputValues.get(0), inputValues.get(1), inputValues.get(2));
+		//check validity
+		if (customTriangle.getTriangleValidity() == true) {
+			//if valid triangle
+
+			//Name
+			System.out.println("Triangle: " + customTriangle.GetName() + " triangle");
+			//Area
+			System.out.println("Area: " + customTriangle.GetArea());
+			customTriangle.printTriangleHeight();
+			customTriangle.printLargestInscribedCircle();
+			customTriangle.printAreaOfTheCircumcircleOfTheTriangle();
+			
+		} else {
+			//error
+			System.out.println("Invalid triangle!");
+			
+		}
+			
+	}
+}
